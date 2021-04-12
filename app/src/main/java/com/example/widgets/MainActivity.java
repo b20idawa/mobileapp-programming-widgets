3a7e1808418ff1349d139f6bb6f61573d3d24f23
b20idawa <b20idawa@student.his.se>
2021-04-12 10:42:02 +0200

    Importerat EditText i MainActivity.java

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.widgets;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.EditText;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
