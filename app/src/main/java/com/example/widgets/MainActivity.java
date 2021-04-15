1199bab20c7ab8b8f9fbba7a5afac6e792a6ec80
b20idawa <b20idawa@student.his.se>
2021-04-15 12:02:41 +0200

    Tagit bort EditText import i MainActivity.java då den var grå vilket betyder att den inte används och därmed inte behövs.

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -4,10 +4,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -21,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText editText = findViewById(R.id.my_edittext);
+
 
         ArrayAdapter<String> adapter=new ArrayAdapter<String>( this,R.layout.list_view_file,R.id.list_view_file_xml,ListData);
         ListView my_listeview=(ListView) findViewById(R.id.listview_item);
